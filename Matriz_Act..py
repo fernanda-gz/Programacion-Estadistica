d044c317656ec577aa34d424ed99c0f171edb6b1
fernanda-gz <159336503+fernanda-gz@users.noreply.github.com>
2024-02-14 19:29:23 -0800

    Add files via upload

---
+++ Matriz_Act..py
@@ -0,0 +1,17 @@
+Matriz = [1,1,1],[1,1,1]
+
+Matriz[0][0]= int(input("Dame el elemento [0][0]: "))
+Matriz[0][1] = int(input("Dame el elemento [0][1]: "))
+Matriz[0][2] = int(input("Dame el elemento [0][2]: "))
+Matriz[1][0] = int(input("Dame el elemento [1][0]: "))
+Matriz[1][1] = int(input("Dame el elemento [1][1]: "))
+Matriz[1][2] = int(input("Dame el elemento [1][2]: "))
+
+
+print("El elemento [0][0] es: ", Matriz[0][0])
+print("El elemento [0][1] es: ", Matriz[0][1])
+print("El elemento [0][2] es: ", Matriz[0][2])
+print("El elemento [1][0] es: ", Matriz[1][0])
+print("El elemento [1][1] es: ", Matriz[1][1])
+print("El elemento [1][2] es: ", Matriz[1][2])
+print(Matriz)
